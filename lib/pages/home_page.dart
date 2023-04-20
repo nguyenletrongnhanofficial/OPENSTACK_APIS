12d6c1b2ad0368e01c6b2caf6694a84afc41f816
Trọng Nhân Nguyễn Lê <trongnhanofficial@gmail.com>
2023-04-20 23:37:17 +0700

    Add Login Get Token

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import '/widgets/instance_widget.dart';
 import '/widgets/network_widget.dart';
 
 class HomePage extends StatefulWidget {
+  final String Token;
+  const HomePage({Key? key, required this.Token}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -18,6 +21,11 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text("Token app vừa nhận được là: "),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(widget.Token),
+            ),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -27,7 +35,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 );
               },
-              child: Text('Go to First Page'),
+              child: Text('Đi tới trang tạo Network'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
@@ -35,11 +43,13 @@ class _HomePageState extends State<HomePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => InstanceWidget(),
+                    builder: (context) => InstanceWidget(
+                      Token: widget.Token,
+                    ),
                   ),
                 );
               },
-              child: Text('Go to Second Page'),
+              child: Text('Đi tới trang tạo Instance'),
             ),
           ],
         ),
